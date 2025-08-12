7dc16a1f20ec4074932a6ef8918a46f3d6cb1c64
CAzhu <1460622019@stu.zafu.edu.cn>
2025-08-12 16:04:15 +0800

    init

---
+++ contracts/NftAuction.sol
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8;
+
+contract NftAuction {
+
+    // 定义结构体
+    struct Auction {
+        
+        address nftAddress;
+        // 拍卖持续时间
+        uint256 duration;
+        // 卖家
+        address payable seller;
+        // 初始价格
+        uint256 startingPrice;
+
+        // 开始时间
+        uint256 startTime;
+        // 结束时间
+        uint256 endTime;
+        uint256 endingPrice;
+       
+        // 最高出价者
+        address highestBidder;
+        // 最高出价
+        uint256 highestBid;
+        bool ended;
+    }
+
+    // 状态变量
+    mapping (uint => Auction) public auctions;
+    uint public nextAuction;
+    // 管理员ID
+    address public admin;
+
+    constructor() { 
+        admin = msg.sender; 
+    }
+ 
+    // 创建拍卖 
+    function createAuction(uint _duration, uint _startingPrice) public { 
+        // 管理员权限
+        require(msg.sender == admin, "Only admin can create auction");
+
+        // 参数验证
+        require(_duration > 1000 * 60, "Duration must be greater than 0");
+        require(_startingPrice > 0, "Starting price must be greater than 0");
+
+        auctions[nextAuction] = Auction({
+            nftAddress: msg.sender,
+            duration: _duration,
+            seller: payable(msg.sender),
+            startingPrice: _startingPrice,
+            startTime: block.timestamp,
+            endTime: block.timestamp + _duration,
+            endingPrice: _startingPrice,
+            highestBidder: address(0),
+            highestBid: 0,
+            ended: false
+        });
+        nextAuction++;
+    }
+
+    // 买家参与买单
+    function placeBid(uint _auctionId) external payable {
+        Auction storage auction = auctions[_auctionId];
+        // 拍卖是否结束
+        require(!auction.ended, "Auction already ended");
+        // 出价是否大于当前最高出价
+        require(msg.value > auction.highestBid, "Bid must be higher than current highest bid");
+        // 出价是否大于初始价格
+        require(msg.value > auction.startingPrice, "Bid must be higher than starting price");
+
+        // 退款
+        if (auction.highestBidder != address(0)) {
+            payable(auction.highestBidder).transfer(auction.highestBid);
+        }
+
+
+        auction.highestBidder = msg.sender;
+        auction.highestBid = msg.value;
+        auction.endingPrice = msg.value;
+        // 
+    }
+}
